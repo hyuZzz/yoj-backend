2a6108f9236cd8190ccc335a05303ea267df9e59
YUUU <894462643@qq.com>
2023-09-27 21:34:31 +0800

    Init

---
+++ src/main/java/com/yuhi/yuoj/controller/PostThumbController.java
@@ -0,0 +1,57 @@
+package com.yuhi.yuoj.controller;
+
+import com.yuhi.yuoj.exception.BusinessException;
+import com.yuhi.yuoj.model.dto.postthumb.PostThumbAddRequest;
+import com.yuhi.yuoj.model.entity.User;
+import com.yuhi.yuoj.service.PostThumbService;
+import com.yuhi.yuoj.service.UserService;
+import com.yuhi.yuoj.common.BaseResponse;
+import com.yuhi.yuoj.common.ErrorCode;
+import com.yuhi.yuoj.common.ResultUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
